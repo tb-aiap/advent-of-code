3846bd11ad7521f9b51f676453a5fca173bddfde
tb-aiap <towboon@gmail.com>
2024-12-02 22:32:28 +0800

    feat: Added solution for day 2 part 2.

--- day_2.py
+++ day_2.py
@@ -1,6 +1,7 @@
 """Solution for day 2 2024."""
 
 MAX_TWO_ADJ_LEVEL = 3
+TOLERATE_BAD_LEVEL = 1
 
 
 def check_increase_safe(arr: list[int]) -> bool:
@@ -23,6 +24,16 @@ def check_decrease_safe(arr: list[int]) -> bool:
     return True
 
 
+def check_with_dampener(arr: list[int]):
+    """For each fail array, try to skip each number and try again."""
+    for i in range(len(arr)):
+        skip_arr = arr[:i] + arr[i + 1 :]
+        if check_increase_safe(skip_arr) or check_decrease_safe(skip_arr):
+            return True
+
+    return False
+
+
 if __name__ == "__main__":
 
     # Part 1
@@ -40,3 +51,19 @@ if __name__ == "__main__":
     print(result)
 
     # Part 2
+    result = 0
+    for report in data:
+        parse_report = report.strip().split(" ")
+        parse_report = [int(i) for i in parse_report]
+
+        if any(
+            [
+                check_increase_safe(parse_report),
+                check_decrease_safe(parse_report),
+            ]
+        ):
+            result += 1
+        elif check_with_dampener(parse_report):
+            result += 1
+
+    print("Part 2", result)
