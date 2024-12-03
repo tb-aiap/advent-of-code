3a2e4ee43c1531abd90c45e38db40c46aa6ca1e8
tb-aiap <towboon@gmail.com>
2024-12-03 15:57:52 +0800

    feat: Added solution for day 3 part 2.

--- day_3.py
+++ day_3.py
@@ -15,3 +15,24 @@ with open("data/input_3.txt", "r") as f:
 result = re.findall(r"mul\(\d*,\d*\)", "".join(data))
 
 print("Part 1", sum(map(parse_mul_str, result)))
+
+# Part 2
+result = re.finditer(r"(mul\(\d*,\d*\))|(don't\(\))|(do\(\))", "".join(data))
+
+answer = 0
+mul_flag = True
+for r in result:
+
+    parsed_str = r.group(0)
+
+    if parsed_str.startswith("mul") and mul_flag:
+        answer += parse_mul_str(parsed_str)
+
+    elif parsed_str.startswith("don't()"):
+        mul_flag = False
+
+    elif parsed_str.startswith("do()"):
+        mul_flag = True
+
+
+print("Part 2", answer)
