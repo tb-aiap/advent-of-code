b873c9a16eb557b9f1a448fecc6d0600feccda3f
tb-aiap <towboon@gmail.com>
2024-12-04 17:15:09 +0800

    feat: Added solution for day 4 part 2.

--- day_4.py
+++ day_4.py
@@ -1,21 +1,20 @@
 """Solution for day 4 2024 in Python."""
 
+from collections import defaultdict
+
 DAY_NO = __file__.rsplit("_", maxsplit=1)[-1].rstrip(".py")
 
 with open(f"data/input_{DAY_NO}.txt", "r") as f:
     data = [s.strip() for s in f]
 
-WORD = "XMAS"
 
 # Part 1 Solution
-
-
 class PuzzleFinder:
-    def __init__(self, puzzle: list[str]):
+    def __init__(self, puzzle: list[str], target: str):
         self.puzzle = puzzle
         self.row_len = len(puzzle)
         self.col_len = len(puzzle[0])
-        self.target = WORD
+        self.target = target
         self.target_len = len(self.target)
 
     def _validate_up_limit(self, r):
@@ -30,6 +29,9 @@ class PuzzleFinder:
     def _validate_left_limit(self, c):
         return c - self.target_len + 1 < 0
 
+    def _return_valid_count(self, chain):
+        return 1 if chain == self.target_len else 0
+
     def search_right(self, r: int, c: int) -> bool:
         """Search right for XMAS."""
 
@@ -39,12 +41,12 @@ class PuzzleFinder:
         chain = 1
         for i in range(1, self.target_len):
             next_col = c + i
-            if chain < 4 and self.puzzle[r][next_col] == self.target[i]:
+            if chain < self.target_len and self.puzzle[r][next_col] == self.target[i]:
                 chain += 1
             else:
                 break
 
-        return 1 if chain == 4 else 0
+        return self._return_valid_count(chain)
 
     def search_left(self, r: int, c: int) -> bool:
         """Search left for SMA'X'."""
@@ -55,12 +57,12 @@ class PuzzleFinder:
         chain = 1
         for i in range(1, self.target_len):
             next_col = c - i
-            if chain < 4 and self.puzzle[r][next_col] == self.target[i]:
+            if chain < self.target_len and self.puzzle[r][next_col] == self.target[i]:
                 chain += 1
             else:
                 break
 
-        return 1 if chain == 4 else 0
+        return self._return_valid_count(chain)
 
     def search_down(self, r: int, c: int) -> bool:
         """Search down for
@@ -76,12 +78,12 @@ class PuzzleFinder:
         chain = 1
         for i in range(1, self.target_len):
             next_row = r + i
-            if chain < 4 and self.puzzle[next_row][c] == self.target[i]:
+            if chain < self.target_len and self.puzzle[next_row][c] == self.target[i]:
                 chain += 1
             else:
                 break
 
-        return 1 if chain == 4 else 0
+        return self._return_valid_count(chain)
 
     def search_up(self, r: int, c: int) -> bool:
         """Search down for
@@ -97,12 +99,12 @@ class PuzzleFinder:
         chain = 1
         for i in range(1, self.target_len):
             next_row = r - i
-            if chain < 4 and self.puzzle[next_row][c] == self.target[i]:
+            if chain < self.target_len and self.puzzle[next_row][c] == self.target[i]:
                 chain += 1
             else:
                 break
 
-        return 1 if chain == 4 else 0
+        return self._return_valid_count(chain)
 
     def search_diagonal_right_down(self, r: int, c: int) -> bool:
         """Search diagonal down for
@@ -119,12 +121,15 @@ class PuzzleFinder:
         for i in range(1, self.target_len):
             next_row = r + i
             next_col = c + i
-            if chain < 4 and self.puzzle[next_row][next_col] == self.target[i]:
+            if (
+                chain < self.target_len
+                and self.puzzle[next_row][next_col] == self.target[i]
+            ):
                 chain += 1
             else:
                 break
 
-        return 1 if chain == 4 else 0
+        return self._return_valid_count(chain)
 
     def search_diagonal_left_down(self, r: int, c: int) -> bool:
         """Search diagonal down for
@@ -141,12 +146,15 @@ class PuzzleFinder:
         for i in range(1, self.target_len):
             next_row = r + i
             next_col = c - i
-            if chain < 4 and self.puzzle[next_row][next_col] == self.target[i]:
+            if (
+                chain < self.target_len
+                and self.puzzle[next_row][next_col] == self.target[i]
+            ):
                 chain += 1
             else:
                 break
 
-        return 1 if chain == 4 else 0
+        return self._return_valid_count(chain)
 
     def search_diagonal_right_up(self, r: int, c: int) -> bool:
         """Search diagonal up for
@@ -163,12 +171,15 @@ class PuzzleFinder:
         for i in range(1, self.target_len):
             next_row = r - i
             next_col = c + i
-            if chain < 4 and self.puzzle[next_row][next_col] == self.target[i]:
+            if (
+                chain < self.target_len
+                and self.puzzle[next_row][next_col] == self.target[i]
+            ):
                 chain += 1
             else:
                 break
 
-        return 1 if chain == 4 else 0
+        return self._return_valid_count(chain)
 
     def search_diagonal_left_up(self, r: int, c: int) -> bool:
         """Search diagonal left up for
@@ -185,22 +196,159 @@ class PuzzleFinder:
         for i in range(1, self.target_len):
             next_row = r - i
             next_col = c - i
-            if chain < 4 and self.puzzle[next_row][next_col] == self.target[i]:
+            if (
+                chain < self.target_len
+                and self.puzzle[next_row][next_col] == self.target[i]
+            ):
                 chain += 1
             else:
                 break
 
-        return 1 if chain == 4 else 0
+        return self._return_valid_count(chain)
+
+
+class XMasPuzzleFinder(PuzzleFinder):
+
+    def __init__(self, puzzle, target):
+        super().__init__(puzzle, target)
+        self._validate_target_is_odd()
+        self.mid_idx = self.target_len // 2
+        self.mid_idx_coord = defaultdict(int)
+
+    def _validate_target_is_odd(self):
+        if self.target_len % 2 == 0:
+            raise ValueError(f"{__class__} expects odd number length.")
+
+    def search_diagonal_right_down(self, r: int, c: int) -> bool:
+        """
+        Search diagonal right down for with memory of the middle coordinate.
+        M..
+        .A.
+        ..S
+        """
+
+        if self._validate_right_limit(c) or self._validate_down_limit(r):
+            return 0
+
+        chain = 1
+        for i in range(1, self.target_len):
+            next_row = r + i
+            next_col = c + i
+            if (
+                chain < self.target_len
+                and self.puzzle[next_row][next_col] == self.target[i]
+            ):
+                chain += 1
+            else:
+                break
+
+        if self._return_valid_count(chain):
+            mid_row = r + self.mid_idx
+            mid_col = c + self.mid_idx
+            self.mid_idx_coord[(mid_row, mid_col)] += 1
+
+        return
+
+    def search_diagonal_left_down(self, r: int, c: int) -> bool:
+        """
+        Search diagonal left down for with memory of the middle coordinate.
+        ..M
+        .A.
+        S..
+        """
+
+        if self._validate_left_limit(c) or self._validate_down_limit(r):
+            return 0
+
+        chain = 1
+        for i in range(1, self.target_len):
+            next_row = r + i
+            next_col = c - i
+            if (
+                chain < self.target_len
+                and self.puzzle[next_row][next_col] == self.target[i]
+            ):
+                chain += 1
+            else:
+                break
+
+        if self._return_valid_count(chain):
+            mid_row = r + self.mid_idx
+            mid_col = c - self.mid_idx
+            self.mid_idx_coord[(mid_row, mid_col)] += 1
+
+        return
+
+    def search_diagonal_right_up(self, r: int, c: int) -> bool:
+        """
+        Search diagonal right up for with memory of the middle coordinate.
+        ..S
+        .A.
+        M..
+        """
+
+        if self._validate_right_limit(c) or self._validate_up_limit(r):
+            return 0
+
+        chain = 1
+        for i in range(1, self.target_len):
+            next_row = r - i
+            next_col = c + i
+            if (
+                chain < self.target_len
+                and self.puzzle[next_row][next_col] == self.target[i]
+            ):
+                chain += 1
+            else:
+                break
+
+        if self._return_valid_count(chain):
+            mid_row = r - self.mid_idx
+            mid_col = c + self.mid_idx
+            self.mid_idx_coord[(mid_row, mid_col)] += 1
+
+        return
+
+    def search_diagonal_left_up(self, r: int, c: int) -> bool:
+        """
+        Search diagonal left up for with memory of the middle coordinate.
+        S..
+        .A.
+        ..M
+        """
+
+        if self._validate_left_limit(c) or self._validate_up_limit(r):
+            return 0
+
+        chain = 1
+        for i in range(1, self.target_len):
+            next_row = r - i
+            next_col = c - i
+            if (
+                chain < self.target_len
+                and self.puzzle[next_row][next_col] == self.target[i]
+            ):
+                chain += 1
+            else:
+                break
+
+        if self._return_valid_count(chain):
+            mid_row = r - self.mid_idx
+            mid_col = c - self.mid_idx
+            self.mid_idx_coord[(mid_row, mid_col)] += 1
+
+        return
 
 
 if __name__ == "__main__":
 
-    finder = PuzzleFinder(data)
+    WORD = "XMAS"
+    finder = PuzzleFinder(data, target=WORD)
 
     result = 0
     for r, s in enumerate(data):
         for c, ss in enumerate(s):
-            if ss == "X":
+            if ss == finder.target[0]:
                 result += finder.search_right(r, c)
                 result += finder.search_left(r, c)
                 result += finder.search_down(r, c)
@@ -211,3 +359,21 @@ if __name__ == "__main__":
                 result += finder.search_diagonal_left_up(r, c)
 
     print("Answer for part 1", result)
+
+    # Part 2 looking for X-MAS
+    # M.S
+    # .A.
+    # M.S
+    MAS = "MAS"
+    finder = XMasPuzzleFinder(data, target=MAS)
+
+    result = 0
+    for r, s in enumerate(data):
+        for c, ss in enumerate(s):
+            if ss == finder.target[0]:
+                finder.search_diagonal_right_down(r, c)
+                finder.search_diagonal_left_down(r, c)
+                finder.search_diagonal_right_up(r, c)
+                finder.search_diagonal_left_up(r, c)
+
+    print("Answer for part 2", list(finder.mid_idx_coord.values()).count(2))
