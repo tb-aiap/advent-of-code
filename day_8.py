9078c711f134fdcbef382319fb0aca8b21243132
tb-aiap <towboon@gmail.com>
2024-12-08 16:56:00 +0800

    Added solution for Day 8 part 2.

--- day_8.py
+++ day_8.py
@@ -5,24 +5,7 @@ from typing import Any
 from utils import get_data, timer
 
 
-def calculate_antinode(
-    point_1: tuple[int, int],
-    point_2: tuple[int, int],
-) -> tuple[tuple[int, int], tuple[int, int]]:
-
-    left = min(point_1, point_2, key=lambda x: x[1])
-    right = max(point_1, point_2, key=lambda x: x[1])
-
-    h_move = left[1] - right[1]
-    v_move = left[0] - right[0]
-
-    antinode_1 = (left[0] + v_move, left[1] + h_move)
-    antinode_2 = (right[0] - v_move, right[1] - h_move)
-
-    return antinode_1, antinode_2
-
-
-def point_within_bound(point: tuple[int, int], grid: list[list[Any]]):
+def is_within_bounds(point: tuple[int, int], grid: list[list[Any]]):
 
     return (
         point[0] >= 0
@@ -32,6 +15,58 @@ def point_within_bound(point: tuple[int, int], grid: list[list[Any]]):
     )
 
 
+def calculate_antinode(
+    point_1: tuple[int, int],
+    point_2: tuple[int, int],
+    grid: list[list[Any]],
+) -> tuple[tuple[int, int], tuple[int, int]]:
+
+    left = min(point_1, point_2, key=lambda x: x[1])
+    right = max(point_1, point_2, key=lambda x: x[1])
+
+    h_move = left[1] - right[1]
+    v_move = left[0] - right[0]
+
+    antinode = [
+        (left[0] + v_move, left[1] + h_move),
+        (right[0] - v_move, right[1] - h_move),
+    ]
+
+    result = [anti for anti in antinode if is_within_bounds(anti, grid)]
+
+    return result
+
+
+def calculate_resonant_harmonic_antinode(
+    point_1: tuple[int, int],
+    point_2: tuple[int, int],
+    grid: list[list[Any]],
+) -> tuple[tuple[int, int], tuple[int, int]]:
+
+    left = min(point_1, point_2, key=lambda x: x[1])
+    right = max(point_1, point_2, key=lambda x: x[1])
+
+    h_move = left[1] - right[1]
+    v_move = left[0] - right[0]
+
+    anti_1, anti_2 = left, right
+
+    results = []
+
+    while is_within_bounds(anti_1, grid) or is_within_bounds(anti_2, grid):
+
+        if is_within_bounds(anti_1, grid):
+            results.append(anti_1)
+
+        if is_within_bounds(anti_2, grid):
+            results.append(anti_2)
+
+        anti_1 = (anti_1[0] + v_move, anti_1[1] + h_move)
+        anti_2 = (anti_2[0] - v_move, anti_2[1] - h_move)
+
+    return results
+
+
 @timer
 def main(data):
 
@@ -39,27 +74,27 @@ def main(data):
 
     for i in range(len(data)):
         for j in range(len(data[0])):
-            if data[i][j] != "." and data[i][j].isalnum():
-                antenna_hash[data[i][j]].append((i, j))
-    # print(antenna_hash)
+            pos = data[i][j]
+            if pos != "." and pos.isalnum():
+                antenna_hash[pos].append((i, j))
 
-    result = []
+    result_1 = []
+    result_2 = []
     for symbol in antenna_hash.keys():
         for p1, p2 in combinations(antenna_hash[symbol], 2):
+            anti_arr = calculate_antinode(p1, p2, data)
+            resonant = calculate_resonant_harmonic_antinode(p1, p2, data)
 
-            antinode_1, antinode_2 = calculate_antinode(p1, p2)
+            result_1.extend(anti_arr)
+            result_2.extend(resonant)
 
-            for anti in [antinode_1, antinode_2]:
-                if point_within_bound(anti, data):
-                    result.append(anti)
-
-    return len(set(result))
+    return len(set(result_1)), len(set(result_2))
 
 
 if __name__ == "__main__":
 
     data = get_data(8)
-    print(data)
 
-    part_1 = main(data)
+    part_1, part_2 = main(data)
     print("Solution for Part 1", part_1)
+    print("Solution for Part 2", part_2)
--- test/test_day_8.py
+++ test/test_day_8.py
@@ -2,7 +2,12 @@
 
 import pytest
 
-from day_8 import calculate_antinode, main, point_within_bound
+from day_8 import (
+    calculate_antinode,
+    calculate_resonant_harmonic_antinode,
+    is_within_bounds,
+    main,
+)
 from utils import get_data
 
 DAY = 8
@@ -13,28 +18,38 @@ def data():
     return get_data(DAY, test_data=True)
 
 
-def test_calculate_antinode():
+def test_calculate_antinode(data):
 
-    data = ((2, 5), (1, 8))
+    points = ((2, 5), (1, 8))
 
-    anti_1, anti_2 = calculate_antinode(*data)
+    result = calculate_antinode(*points, data)
 
-    assert anti_1 == (3, 2)
-    assert anti_2 == (0, 11)
+    assert result[0] == (3, 2)
+    assert result[1] == (0, 11)
 
 
 def test_point_within_bound(data):
 
-    assert point_within_bound((1, 1), data)
-    assert not point_within_bound((-1, 1), data)
-    assert not point_within_bound((1, -1), data)
-    assert not point_within_bound((12, 5), data)
-    assert not point_within_bound((11, 12), data)
-    assert point_within_bound((11, 11), data)
+    assert is_within_bounds((1, 1), data)
+    assert not is_within_bounds((-1, 1), data)
+    assert not is_within_bounds((1, -1), data)
+    assert not is_within_bounds((12, 5), data)
+    assert not is_within_bounds((11, 12), data)
+    assert is_within_bounds((11, 11), data)
 
 
 def test_main_part_1(data):
 
-    part1 = main(data)
+    part1, part2 = main(data)
 
     assert part1 == 14
+    assert part2 == 34
+
+
+def test_calculate_resonant_harmonic_antinode(data):
+
+    points = ((8, 8), (8, 9))
+
+    result = calculate_resonant_harmonic_antinode(*points, data)
+
+    assert len(result) == 12
