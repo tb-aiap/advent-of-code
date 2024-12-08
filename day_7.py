b152bc7f6181bfc9bd35da188afb265113aff7fc
tb-aiap <towboon@gmail.com>
2024-12-08 22:01:33 +0800

    refactor: Remove duplicated code.

--- day_7.py
+++ day_7.py
@@ -15,39 +15,25 @@ def split_data(data: list[str]) -> tuple[int, int]:
     return ans, inputs
 
 
-def brute_force_recursive(left, arr: list[int], ans):
+def brute_force_recursive(left, arr: list[int], ans, concat: bool = False):
 
-    if not arr and left != ans:
+    if not arr:
+        return left == ans
+    elif left > ans:
         return False
-    elif left == ans:
-        return True
 
     right = arr[0]
     remaining_arr = arr[1:]
 
-    multiply = brute_force_recursive(left * right, remaining_arr, ans)
-    adding = brute_force_recursive(left + right, remaining_arr, ans)
+    if concat:
+        concatenate = brute_force_recursive(
+            int(str(left) + str(right)), remaining_arr, ans, concat=concat
+        )
 
-    return adding or multiply
+    adding = brute_force_recursive(left + right, remaining_arr, ans, concat=concat)
+    multiply = brute_force_recursive(left * right, remaining_arr, ans, concat=concat)
 
-
-def brute_force_recursive_again(left, arr: list[int], ans):
-
-    if not arr and left != ans:
-        return False
-    elif left == ans:
-        return True
-
-    right = arr[0]
-    remaining_arr = arr[1:]
-
-    multiply = brute_force_recursive_again(left * right, remaining_arr, ans)
-    adding = brute_force_recursive_again(left + right, remaining_arr, ans)
-    concat = brute_force_recursive_again(
-        int(str(left) + str(right)), remaining_arr, ans
-    )
-
-    return adding or multiply or concat
+    return adding or multiply or (concatenate if concat else False)
 
 
 @timer
@@ -55,30 +41,21 @@ def main(data):
 
     ans, qns = split_data(data)
     result = 0
+    part_2 = 0
     for i in range(len(qns)):
         if brute_force_recursive(qns[i][0], qns[i][1:], ans[i]):
             result += ans[i]
 
-    return result
+        elif brute_force_recursive(qns[i][0], qns[i][1:], ans[i], concat=True):
+            part_2 += ans[i]
 
-
-@timer
-def main_part_2(data):
-
-    ans, qns = split_data(data)
-    result = 0
-    for i in range(len(qns)):
-        if brute_force_recursive_again(qns[i][0], qns[i][1:], ans[i]):
-            result += ans[i]
-
-    return result
+    return result, result + part_2
 
 
 if __name__ == "__main__":
 
     data = get_data(day_no=7)
-    part_1 = main(data)
-    print("Solution for Part 1", part_1)
+    part_1, part_2 = main(data)
 
-    part_2 = main_part_2(data)
+    print("Solution for Part 1", part_1)
     print("Solution for Part 2", part_2)
--- test/test_day_7.py
+++ test/test_day_7.py
@@ -1,8 +1,8 @@
-"""Test for day 6 inputs."""
+"""Test for day 7 inputs."""
 
 import pytest
 
-from day_7 import brute_force_recursive_again, main, main_part_2
+from day_7 import main
 from utils import get_data
 
 DAY = 7
@@ -13,20 +13,14 @@ def data():
     return get_data(DAY, test_data=True)
 
 
-def test_brute_force_recursive_again():
-
-    assert brute_force_recursive_again(15, [6], 156)
-    assert brute_force_recursive_again(17, [8, 14], 192)
-    assert brute_force_recursive_again(6, [8, 6, 15], 7290)
-
-
 def test_main(data):
 
-    part_1 = main(data)
+    part_1, part_2 = main(data)
     assert part_1 == 3749
+    assert part_2 == 11387
 
 
-def test_main_part_2(data):
+# def test_main_part_2(data):
 
-    ans = main_part_2(data)
-    assert ans == 11387
+#     ans = main_part_2(data)
+#     assert ans == 11387
