2bb6d3d60a7fff38bab6ac53b71fc01f8b3888b8
tb-aiap <towboon@gmail.com>
2024-12-31 18:24:54 +0800

    feat: Added initial solution for Day 18 part 1 and 2.

---
+++ day_18.py
@@ -0,0 +1,76 @@
+"""Solution for Day 18 2024."""
+
+from collections import deque
+
+import utils
+
+
+def get_coords(data: list[str]):
+    for s in data:
+        yield map(int, s.split(","))
+
+
+def bfs(start: tuple[int, int], end: tuple[int, int], grid: list[list[str]]):
+
+    path = [(start)]
+    visited = set()
+    queue = deque()
+    queue.append((*start, path))
+
+    while queue:
+        r, c, curr_path = queue.popleft()
+
+        if (r, c) == end:
+            print("Path include (0,0)", len(curr_path), curr_path)
+            return len(curr_path)
+
+        for dir in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            nr, nc = r + dir[0], c + dir[1]
+            if (
+                (nr, nc) not in visited
+                and utils.is_within_bounds((nr, nc), grid)
+                and grid[nr][nc] == "."
+            ):
+                visited.add((nr, nc))
+                new_path = curr_path.copy()
+                new_path.append((nr, nc))
+                queue.append((nr, nc, new_path))
+
+
+@utils.timer
+def main(data):
+
+    coords = get_coords(data)
+
+    if len(data) < 1024:
+        size, b, end = 7, 12, (6, 6)
+    else:
+        size, b, end = 71, 1024, (70, 70)
+
+    grid = [["."] * size for _ in range(size)]
+
+    for _ in range(b):
+        x, y = next(coords)
+        grid[y][x] = "#"
+
+    part_1 = bfs((0, 0), end, grid) - 1
+
+    while True:
+        x, y = next(coords)
+        grid[y][x] = "#"
+        if bfs((0, 0), end, grid) is None:
+            break
+
+    part_2 = f"{x},{y}"
+
+    return part_1, part_2
+
+
+if __name__ == "__main__":
+
+    DAY = __file__.rsplit("_", maxsplit=1)[-1].rstrip(".py")
+    data = utils.get_data(DAY)
+    part_1, part_2 = main(data)
+
+    utils.print_solution(2024, DAY, "Part 1", part_1)
+    utils.print_solution(2024, DAY, "Part 2", part_2)
---
+++ test/test_day_18.py
@@ -0,0 +1,23 @@
+"""Test for day 18 inputs."""
+
+import importlib
+
+import pytest
+
+from utils import get_data
+
+DAY = __file__.rsplit("_", maxsplit=1)[-1].rstrip(".py")
+MAIN = importlib.import_module(f"day_{DAY}")
+
+
+@pytest.fixture()
+def data():
+    return get_data(DAY, test_data=True)
+
+
+def test_main(data):
+
+    part1, part2 = MAIN.main(data)
+
+    assert part1 == 22
+    assert part2 == "6,1"
