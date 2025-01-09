d5b365f99889317589743bf71893d0ab74d57a3e
tb-aiap <towboon@gmail.com>
2025-01-09 15:51:03 +0800

    feat: Added __main__ file as main entry point.

---
+++ __main__.py
@@ -0,0 +1,41 @@
+"""Main Entry point for Advent of Code Solutions."""
+
+import argparse
+import importlib
+
+import utils
+
+
+def get_argparse() -> argparse.ArgumentParser:
+    """Parse the required arguments for AOC solutions."""
+    parser = argparse.ArgumentParser(description="Advent of Code CLI Options")
+    parser.add_argument("-t", default=False, action=argparse.BooleanOptionalAction)
+    parser.add_argument("year", type=int)
+    parser.add_argument("day", type=int)
+
+    return parser
+
+
+def main(parser: argparse.ArgumentParser) -> None:
+    """Main Entry point to AOC."""
+    args = parser.parse_args()
+    year = args.year
+    day = args.day
+    test_data = args.t
+
+    if test_data:
+        print("Running Test Data")
+
+    solve = importlib.import_module(f"{year}.day_{day:02d}")
+
+    data = utils.get_data(day, test_data=test_data)
+    p1, p2 = solve.main(data)
+
+    utils.print_solution(year, day, "Part 1", p1)
+    utils.print_solution(year, day, "Part 2", p2)
+
+
+if __name__ == "__main__":
+
+    parser = get_argparse()
+    main(parser)
