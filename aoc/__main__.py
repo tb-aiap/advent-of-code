116f8ebd6a4a36589bf1c9298b945e8807d8c139
tb-aiap <towboon@gmail.com>
2025-07-14 21:50:15 +0800

    refactor: hook up main and year folder

--- aoc/__main__.py
+++ aoc/__main__.py
@@ -1,5 +1,14 @@
+import importlib
+import sys
+from pathlib import Path
+
 import typer
 from typing_extensions import Annotated
+
+add_path = Path(__file__, f"../..").resolve()
+sys.path.append(add_path.as_posix())
+
+import utils
 
 app = typer.Typer(
     help="CLI for Advent of Code management over multiple years.",
@@ -36,8 +45,16 @@ def run(
         year (int): Year of the AOC
         day (int): Which day of the AOC
     """
+
+    solve = importlib.import_module(f"{year}.day_{day:02d}")
     print(submit)
     print(f"Testing {year} and {day}")
+
+    data = utils.get_data(day, test_data=submit)
+    p1, p2 = solve.main(data)
+
+    utils.print_solution(year, day, "Part 1", p1)
+    utils.print_solution(year, day, "Part 2", p2)
 
 
 if __name__ == "__main__":
