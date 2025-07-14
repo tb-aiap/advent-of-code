cf6dba8b58e00372bc1c4f7e0febb131ac019ca9
tb-aiap <towboon@gmail.com>
2025-07-14 21:32:30 +0800

    refactor: reshuffle run and submit command

--- aoc/__main__.py
+++ aoc/__main__.py
@@ -1,6 +1,11 @@
 import typer
+from typing_extensions import Annotated
 
-app = typer.Typer(help="CLI for Advent of Code management over multiple years.",no_args_is_help=True)
+app = typer.Typer(
+    help="CLI for Advent of Code management over multiple years.",
+    no_args_is_help=True,
+)
+
 
 @app.command()
 def retrieve(year: int, day: int):
@@ -11,25 +16,17 @@ def retrieve(year: int, day: int):
 
     Args:
         year (int): Year of the AOC
-        day (int): Which day of the AOC 
+        day (int): Which day of the AOC
     """
     print(f"Retrieving {year} and {day}")
 
-@app.command()
-def test(year: int, day: int):
-    """
-    Test AOC sample data for year and day.
-
-    Remember to manually copy and paste the data.
-
-    Args:
-        year (int): Year of the AOC
-        day (int): Which day of the AOC 
-    """
-    print(f"Testing {year} and {day}")
 
 @app.command()
-def run(year: int, day: int):
+def run(
+    year: int,
+    day: int,
+    submit: Annotated[bool, typer.Option(help="Use actual data to run")] = False,
+):
     """
     Run and output actual data against the solution. Manually submit it in website.
 
@@ -37,9 +34,11 @@ def run(year: int, day: int):
 
     Args:
         year (int): Year of the AOC
-        day (int): Which day of the AOC 
+        day (int): Which day of the AOC
     """
+    print(submit)
     print(f"Testing {year} and {day}")
+
 
 if __name__ == "__main__":
     app()
