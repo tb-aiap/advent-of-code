def3bb1bfc60690706bee603a05794926c223a4c
tb-aiap <towboon@gmail.com>
2025-07-15 23:42:39 +0800

    refactor: typer initial commit create and run done

--- __main__.py
+++ __main__.py
@@ -1,37 +1,91 @@
 """Main Entry point for Advent of Code Solutions."""
 
-import argparse
 import importlib
+from pathlib import Path
 
+import typer
 from rich.console import Console
+from typing_extensions import Annotated
 
+import retrieve_data
 import utils
+
+CONFIG = {
+    "work_dir": Path.cwd(),
+}
+
+app = typer.Typer(
+    help="CLI for Advent of Code management over multiple years.",
+    no_args_is_help=True,
+)
 
 console = Console()
 
-def get_argparse() -> argparse.ArgumentParser:
-    """Parse the required arguments for AOC solutions."""
-    parser = argparse.ArgumentParser(description="Advent of Code CLI Options")
-    parser.add_argument("-t", default=False, action=argparse.BooleanOptionalAction)
-    parser.add_argument("year", type=int)
-    parser.add_argument("day", type=int)
 
-    return parser
+@app.command()
+def create(year: int):
+    """Create folder and respective py files for selected year.
+
+    Args:
+        year (int): YEAR in YYYY format
+    """
+    work_dir = CONFIG["work_dir"]
+    console.print(f"Creating folder for {year} in {work_dir}", style="bold")
+    year_folder = Path(work_dir, str(year))
+
+    if year_folder.is_dir():
+        console.print("Folder alrdy exist, skipping..", style="red")
+        return
+
+    console.print(f"creating folder {year} and the days file...", style="italic green")
+    year_folder.mkdir()
+
+    Path(year_folder, "__init__.py").touch()
+
+    for i in range(1, 26):
+        code_template = retrieve_data.main_code_template(i, year=year)
+        day_file = Path(year_folder, f"day_{i:02d}.py")
+        if not day_file.is_file():
+            with open(day_file.as_posix(), mode="w") as file:
+                file.write(code_template)
+        else:
+            console.print("File alrdy exist, skipping..", style="red")
 
 
-def main(parser: argparse.ArgumentParser) -> None:
-    """Main Entry point to AOC."""
-    args = parser.parse_args()
-    year = args.year
-    day = args.day
-    test_data = args.t
+@app.command()
+def retrieve(year: int, day: int):
+    """
+    Retrieve AOC data from provided year and day.
 
-    if test_data:
-        console.print("Running Test Data", style="bold blue")
+    Test data needs manual copy and pasting though...
 
+    Args:
+        year (int): Year of the AOC
+        day (int): Which day of the AOC
+    """
+    print(f"Retrieving {year} and {day}")
+
+
+@app.command()
+def run(
+    year: int,
+    day: int,
+    test: Annotated[bool, typer.Option("-s", help="Use actual data to run")] = True,
+):
+    """
+    Run and output actual data against the solution. Manually submit it in website.
+
+    Submiited solution is cached in folder if duplicate exists.
+
+    Args:
+        year (int): Year of the AOC
+        day (int): Which day of the AOC
+    """
     solve = importlib.import_module(f"{year}.day_{day:02d}")
+    if test:
+        console.print(f"Testing {year} and {day}", style="blue bold")
 
-    data = utils.get_data(day, test_data=test_data)
+    data = utils.get_data(day, test_data=test)
     p1, p2 = solve.main(data)
 
     utils.print_solution(year, day, "Part 1", p1)
@@ -39,6 +93,4 @@ def main(parser: argparse.ArgumentParser) -> None:
 
 
 if __name__ == "__main__":
-
-    parser = get_argparse()
-    main(parser)
+    app()
