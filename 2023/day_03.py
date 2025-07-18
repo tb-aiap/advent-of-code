59abd87f2226c22cd699f589ce8a000ff1feb0a8
tb-aiap <towboon@gmail.com>
2025-07-18 12:49:05 +0800

    feat: Solution for 2023 day 3

---
+++ 2023/day_03.py
@@ -0,0 +1,112 @@
+"""Solution for Day 3 2023."""
+
+from collections import defaultdict
+from functools import reduce
+from operator import mul
+
+import utils
+
+# for each row, for each number scan 9 diretions for symbol.
+
+scan_direction = [
+    # row, col
+    (-1, 0),  # up 0
+    (-1, 1),  # up right
+    (0, 1),  # 0 right
+    (1, 1),  # right down
+    (1, 0),  # down 0
+    (1, -1),  # down left
+    (0, -1),  # 0 left
+    (-1, -1),  # left up
+]
+
+
+def number_near_parts(data: list[list[str]], r: int, c: int) -> bool:
+    """Search surrounding area for parts"""
+    for dir in scan_direction:
+        dir_r = r + dir[0]
+        dir_c = c + dir[1]
+        if not (0 <= dir_r < len(data) and 0 <= dir_c < len(data[0])):
+            continue  # out of bounds
+        if not data[dir_r][dir_c].isnumeric() and data[dir_r][dir_c] != ".":
+            # this number is near a part
+            return True
+    return False
+
+
+def number_near_gear(data: list[list[str]], r: int, c: int) -> tuple[int]:
+    """Search surrounding area for parts"""
+    for dir in scan_direction:
+        dir_r = r + dir[0]
+        dir_c = c + dir[1]
+        if not (0 <= dir_r < len(data) and 0 <= dir_c < len(data[0])):
+            continue  # out of bounds
+        if data[dir_r][dir_c] == "*":
+            return dir_r, dir_c
+    return -1, -1
+
+
+def solve_1(data: list[list[str]]) -> int:
+    """Solution for part 1."""
+    result = 0
+    for r in range(len(data)):
+        number_holder = ""
+        is_near_parts = False
+        for c in range(len(data[r])):
+            val = data[r][c]
+            if val.isnumeric():
+                number_holder += val
+                if not is_near_parts:
+                    is_near_parts = number_near_parts(data, r, c)
+            else:
+                if number_holder and is_near_parts:
+                    result += int(number_holder)
+                    is_near_parts = False
+                number_holder = ""
+
+        if number_holder and is_near_parts:
+            result += int(number_holder)
+            is_near_parts = False
+
+    return result
+
+
+def solve_2(data: list[list[str]]) -> dict[tuple[int], list[int]]:
+    """Solution for part 2."""
+    gear_ratio = defaultdict(list)
+    for r in range(len(data)):
+        number_holder = ""
+        gear_coord = -1, -1
+        for c in range(len(data[r])):
+            val = data[r][c]
+            if val.isnumeric():
+                number_holder += val
+                if gear_coord == (-1, -1):
+                    gear_coord = number_near_gear(data, r, c)
+            else:
+                if number_holder and gear_coord != (-1, -1):
+                    gear_ratio[gear_coord].append(int(number_holder))
+                    gear_coord = -1, -1
+                number_holder = ""
+        if number_holder and gear_coord != (-1, -1):
+            gear_ratio[gear_coord].append(int(number_holder))
+            gear_coord = -1, -1
+    return gear_ratio
+
+
+@utils.timer
+def main(data):
+
+    part_1 = solve_1(data)
+    part_2 = sum(reduce(mul, v, 1) for v in solve_2(data).values() if len(v) == 2)
+
+    return part_1, part_2
+
+
+if __name__ == "__main__":
+
+    data = utils.get_data(3)
+    part_1, part_2 = main(data)
+
+    utils.print_solution(2023, 3, "Part 1", part_1)
+    utils.print_solution(2023, 3, "Part 2", part_2)
